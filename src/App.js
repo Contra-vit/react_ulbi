680ad1864c6fea755a1ef7a5e9248bad38f750f2
Contra-vit <40683716+Contra-vit@users.noreply.github.com>
2022-10-06 13:14:06 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Counter from './Components/Counter';
+import ClassCounter from './Components/ClassCounter';
+import    './stylies/App.css';
+import PostList from './Components/PostList';
 
 function App() {
+const [posts, setPost] = useState( [
+  {id:1, title: 'JavaScript1', body:'Description'},
+  {id:2, title: 'JavaScript2', body:'Description'},
+  {id:3, title: 'JavaScript3', body:'Description'},
+])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <PostList posts = {posts}/> 
+      {/* <PostItem post={{id:1, title: 'JavaScript', body:'Description'}} /> */}
+
+<Counter/>
+ <br/>
+<ClassCounter/>
+
+  
+  
+     
+  </div>
+   );
 }
 
 export default App;
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import   './Counter.css';
+
+const Counter= () => {
+    const [count, setCount] = useState(0);
+    const [value, setValue] = useState('input text');
+console.log(setValue)
+console.log(setCount)
+
+    function increment() {
+        setCount(count + 1)
+    }
+
+    function decrement(){
+        setCount(count - 1)
+    }
+     
+    return (
+        <div className='counter'>
+            <input 
+            type="text"
+            value= {value}
+            onChange={event => setValue(event.target.value)} 
+            />
+            <h1>{count}</h1> 
+            <h1>{value}</h1>
+            <button onClick={increment}>Increnent</button>
+            <button onClick= {decrement}>Decrement</button>
+        </div>
+    )
+}
+export default Counter;
